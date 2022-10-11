1600527f89556e5cea363092c79a2f0daf2043f5
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-10-11 21:27:25 +0700

    new

---
+++ Lab09/Lab09_03/src/Customer.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private ArrayList acct;
+    
+    public Customer(){
+        this("", "");
+    }
+    public Customer(String firstName,String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        acct = new ArrayList();
+    }
+    public Account getAccount(int index){
+        return (Account)this.acct.get(index);
+    }
+    public void addAccount(Account acct){
+        this.acct.add(acct);
+    }
+    public int getNumOfAccount(){
+        return acct.size();
+    }
+    public String toString(){
+        return this.firstName+" "+this.lastName+" has "+acct.size()+" account.";
+    }
+}
