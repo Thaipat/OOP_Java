66e6cb2124aab08f6f50533a73dc67c06090ab60
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-11-09 02:02:00 +0700

    new

--- Lab12/Lab12_02/src/MyClock.java
+++ Lab12/Lab12_02/src/MyClock.java
@@ -2,28 +2,52 @@ import java.util.Calendar;
 import javax.swing.*;
 import java.awt.*;
 public class MyClock extends JLabel implements Runnable{
-    int i;
+    private int time;
+    private boolean paused;
     public void run(){
         try{
             while(true){
-                Calendar d = Calendar.getInstance();
-                int sec = d.get(Calendar.SECOND);
-                int min = d.get(Calendar.MINUTE);
-                int hour = d.get(Calendar.HOUR_OF_DAY);
+                checkPause();
                 
-//                int sec = i%60;
-//                int min = (i/60)%60;
-//                int hour = (i/3600)%24;
+//                Calendar d = Calendar.getInstance();
+//                int sec = d.get(Calendar.SECOND);
+//                int min = d.get(Calendar.MINUTE);
+//                int hour = d.get(Calendar.HOUR_OF_DAY);
+                
+                int sec = time%60;
+                int min = (time/60)%60;
+                int hour = (time/3600)%24;
                 
                 this.setEnabled(false);
                 this.setBorder(null);
                 this.setFont(new Font("Tahoma",Font.BOLD, 80));
                 this.setText(String.format("%02d:%02d:%02d", hour, min, sec));
                 Thread.sleep(1000);
-                i++;
+                time++;
             }
         }catch(Exception e){
             System.out.println(e);
         }
+    }
+    
+    public void pauseThread(){
+        paused = true;
+        System.out.println("Paused");
+    }
+    
+    public void checkPause(){
+        while(paused){
+            try{
+                this.wait();
+            }catch(Exception e){
+//                e.printStackTrace();
+            }
+        }
+    }
+    
+    public synchronized void resumeThread(){
+        paused = false;
+        this.notify();
+        System.out.println("UnPaused");
     }
 }
--- Lab12/Lab12_02/src/MyFrame.java
+++ Lab12/Lab12_02/src/MyFrame.java
@@ -1,14 +1,19 @@
 import java.awt.*;
 import javax.swing.*;
-public class MyFrame {
+import java.awt.event.*;
+public class MyFrame implements MouseListener{
     private JFrame window;
     private MyClock clock;
     private Thread t;
+    private int checkPause = 0;
+
     public MyFrame(){
         window = new JFrame();
         clock = new MyClock();
         t = new Thread(clock);
         t.start();
+        
+        clock.addMouseListener(this);
         
         window.setLayout(new FlowLayout());
         window.add(clock);
@@ -21,4 +26,19 @@ public class MyFrame {
     public static void main(String[] args){
         new MyFrame();
     }
+    
+    public synchronized void mouseClicked(MouseEvent ev){
+        if(checkPause%2==0){
+            clock.pauseThread();
+        }
+        else{
+            clock.resumeThread();
+        }
+        checkPause++;
+    }
+    public void mouseEntered(MouseEvent ev){}
+    public void mouseExited(MouseEvent ev){}
+    public void mousePressed(MouseEvent ev){}
+    public void mouseReleased(MouseEvent ev){}
+    
 }
